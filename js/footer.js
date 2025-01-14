12bebf7e8a4430a80a6934e7c957f001ba6cabcf
jgcamiloaga <jcamiloc148@gmail.com>
2025-01-14 17:19:28 -0500

    Elimina enlaces redundantes en el pie de página

--- js/footer.js
+++ js/footer.js
@@ -48,9 +48,7 @@ function loadFooter() {
                     <li><a href="inicio.html" class="menu-link">Inicio</a></li>
                     <li><a href="Nosotros.html" class="menu-link">Nosotros</a></li>
                     <li><a href="RedesSociales.html" class="menu-link">Redes Sociales</a></li>
-                    <li><a href="Contactanos.html" class="menu-link">Ubícanos</a></li>
                     <li><a href="Contactanos.html" class="menu-link">Contáctanos</a></li>
-                    <li><a href="Contactanos.html" class="menu-link">Libro de Reclamaciones</a></li>
                     <li><a href="carrito.html" class="menu-link">Carrito</a></li>
                 </ul>
             </div>
